9fe4c86819d49d46f6ac845d4c409b84c1672b55
8dreams <65988224+8dreams@users.noreply.github.com>
2020-05-27 15:27:17 +0800

    Create ideologies.js

---
+++ ideologies.js
@@ -0,0 +1,173 @@
+ideologies = [
+    {
+        "name": "English Socialism",
+        "stats": {
+            "econ": 50,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Stirnerist Egoism",
+        "stats": {
+            "econ": 70,
+            "dipl": 100,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Rational Egoism",
+        "stats": {
+            "econ": 30,
+            "dipl": 100,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Transhumanist Ego Theocratic Imperialism",
+        "stats": {
+            "econ": 60,
+            "dipl": 10,
+            "govt": 100,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Social Darwinism",
+        "stats": {
+            "econ": 0,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 0
+        }
+    },
+    {
+        "name": "Communalism",
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Primalism",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 0
+        }
+    },
+    {
+        "name": "Ego-Primalism",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 0
+        }
+    },
+    {
+        "name": "Totalitarian Primalism",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "name": "Post-Humanism",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Communal Post-Humanism",
+        "stats": {
+            "econ": 100,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Ego Post-Humanism",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 100,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Hivemind Collectivism",
+        "stats": {
+            "econ": 100,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Kraterocracy",
+        "stats": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "name": "Avaritionism",
+        "stats": {
+            "econ": 0,
+            "dipl": 100,
+            "govt": 100,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Soulism",
+        "stats": {
+            "econ": 80,
+            "dipl": 100,
+            "govt": 100,
+            "scty": 100
+        }
+    },
+    {
+        "name": "Realism",
+        "stats": {
+            "econ": 50,
+            "dipl": 50,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Anti-Realism",
+        "stats": {
+            "econ": 50,
+            "dipl": 0,
+            "govt": 50,
+            "scty": 50
+        }
+    },
+    {
+        "name": "Egoism",
+        "stats": {
+            "econ": 50,
+            "dipl": 100,
+            "govt": 100,
+            "scty": 50
+        }
+    }
+];
