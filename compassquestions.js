7602b4c927e8c43032ef6a21db0194a67ef8ab75
8dreams <65988224+8dreams@users.noreply.github.com>
2020-06-03 10:01:28 +0800

    Create compassquestions.js

---
+++ compassquestions.js
@@ -0,0 +1,57 @@
+questions =  [
+    //Left-Right
+    {"id": 0, "question": "I should be able to control my own property.", "effects": {"right": 1}},
+    {"id": 2, "question": "Theft should be considered a 'victimless crime'.", "effects": {"right": -1}},
+    {"id": 3, "question": "Unjust civilizations should be toppled, by nuclear force if necessary.", "effects": {"right": -1}},
+    {"id": 4, "question": "A collectivist utopia has been proven unsuccessful in the past.", "effects": {"right": 1}},
+    {"id": 5, "question": "My information, my choice.", "effects": {"right": 1}},
+    {"id": 6, "question": "The idea of currency itself is proven unnecessary.", "effects": {"right": -1}},
+    {"id": 7, "question": "1984 is Capitalist Propaganda.", "effects": {"right": -1}},
+    {"id": 8, "question": "There is no reason to weaken the strong, since they worked so hard for their strength.", "effects": {"right": -2}},
+    {"id": 9, "question": "Basic utilities like knowledge and air should be publicly owned.", "effects": {"right": -1}},
+    {"id": 10, "question": "The 2nd Amendment of the United States Constitution should be expanded to include any weapons.", "effects": {"right": 1}},
+    {"id": 11, "question": "The bad circumstances of the weak are the results of their own actions.", "effects": {"right": 1}},
+    {"id": 12, "question": "There should be competing Worldwide Organizations.", "effects": {"right": 1}},
+    {"id": 13, "question": "People who claim that knowledge is private are selfish.", "effects": {"right": -1}},
+    {"id": 14, "question": "The idea of property itself is selfish.", "effects": {"right": -1}},
+    {"id": 15, "question": "If a member of society refuses to share their property, violence should be used.", "effects": {"right": -1}},
+   
+    //Auth-Lib
+    {"id": 16, "question": "Totalitarian figures, if morally correct, are a good thing for society.", "effects": {"auth": 1}},
+    {"id": 17, "question": "The world of 1984 is a ideal world theoretically and practically.": {"auth": 1}},
+    {"id": 18, "question": "Only the community or collective can make a change, not the individual.", "effects": {"auth": 1}},
+    {"id": 19, "question": "Governments are spooks.", "effects": {"auth": -1}},
+    {"id": 20, "question": "We should kill a person rather than convincing them if they strongly disagree with us.", "effects": {"auth": 1}},
+    {"id": 21, "question": "Even when protesting an totalitarian government, nukes are not acceptable.", "effects": {"auth": 1}},
+    {"id": 22, "question": "The very concept of the NAP is unworkable.", "effects": {"auth": -1}},
+    {"id": 23, "question": "The modern societal hierarchy is a spook.", "effects": {"auth": -1}},
+    {"id": 24, "question": "The fact is, people are Egoistic, and accepting it is what we should do.", "effects": {"auth": -1}},
+    {"id": 25, "question": "All individuals should be questioned, to see if they are loyal to society.", "effects": {"auth": 2}},
+    {"id": 26, "question": "The fact is, people are Egoistic, and accepting it is what we should do.", "effects": {"auth": -1}},
+    {"id": 27, "question": "Only the community or collective can make a change, not the individual.", "effects": {"auth": 1}},
+    {"id": 28, "question": "Individuals have no value of their own.", "effects": {"auth": -1}},
+    {"id": 29, "question": "The fact is, people are Egoistic, and accepting it is what we should do.", "effects": {"auth": -1}},
+    {"id": 30, "question": "Monitoring people's everyday lives produces stability and prosperity.", "effects": {"auth": 1}},
+    
+    //Prog-Con
+    {"id": 31, "question": "Life in the Middle Ages was significantly better.", "effects": {"prog": -1}},
+    {"id": 32, "question": "Ever since the Renaissance and the Enlightenment, humanity has head the wrong direction.", "effects": {"prog": 1}},
+    {"id": 33, "question": "The transition from Amniotes to Mammals was a incorrect move.", "effects": {"prog": -2}},
+    {"id": 34, "question": "Technological development will soon destroy humanity.", "effects": {"prog": 1}},
+    {"id": 35, "question": "Humanity as a species should not have existed.", "effects": {"prog": -1}},
+    {"id": 36, "question": "Rather than frustrating about Global Warming, we should just wait until technology solves the problem.", "effects": {"prog": 1}},
+    {"id": 37, "question": "Transhumanism should be implemented to combat future technology.", "effects": {"prog": 1}},
+    {"id": 38, "question": "Culture itself is a form of peer pressure.", "effects": {"prog": 1}},
+    {"id": 39, "question": "Humans will be replaced by our own inventions, and there's no escape.", "effects": {"prog": 1}},
+    {"id": 40, "question": "Humanism was wrong. We should appreciate work done by robots instead.", "effects": {"prog": 1}},
+    {"id": 41, "question": "Modern Wars are far more meaningless, when compared to Medieval Wars.", "effects": {"prog": -1}},
+    {"id": 42, "question": "Post-Modernism is a bad ideology that aims to remove any meaning in existence.", "effects": {"prog": -1}},
+    {"id": 43, "question": "Ancient Egyptians did a huge mistake by combining the Upper and Lower Kingdoms in 3000BC.", "effects": {"prog": 1}},
+    {"id": 44, "question": "A surgery should be done to remove Humanity painlessly to avoid painful genocide in the future by robotic life.", "effects": {"prog": 1}},
+    {"id": 45, "question": "We should build a planetary wall to avoid Martian invasion.", "effects": {"prog": 1}}
+    
+];
+    
+    
+    
+    
